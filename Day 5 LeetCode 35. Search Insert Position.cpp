a164c1789ab6fb68b31ffffa8f5ac2de9e9ab78f
shivakant kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-03-20 20:01:51 +0530

    Create Day 5 LeetCode 35. Search Insert Position.cpp

---
+++ Day 5 LeetCode 35. Search Insert Position.cpp
@@ -0,0 +1,21 @@
+/*
+35. Search Insert Position
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+class Solution {
+public:
+    int binarySearch(vector<int>&arr,int l , int r , int target){
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(arr[mid]==target)return mid;
+            else if(arr[mid]>target)r=mid-1;
+            else l=mid+1;
+        }
+        return l;
+    }
+    int searchInsert(vector<int>& nums, int target) {
+        return binarySearch(nums,0,nums.size()-1,target);
+    }
+};
