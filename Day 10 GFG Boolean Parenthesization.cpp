af025f70cd479f4c8289f60a707d5c08028f5f85
shivakant kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-03-25 21:31:32 +0530

    Create Day 10 GFG Boolean Parenthesization.cpp

---
+++ Day 10 GFG Boolean Parenthesization.cpp
@@ -0,0 +1,63 @@
+/*
+Boolean Parenthesization
+Difficulty: HardAccuracy: 20.15%Submissions: 131K+Points: 8Average Time: 30m
+You are given a boolean expression s containing
+    'T' ---> true
+    'F' ---> false 
+and following operators between symbols
+   &   ---> boolean AND
+    |   ---> boolean OR
+   ^   ---> boolean XOR
+Count the number of ways we can parenthesize the expression so that the value of expression evaluates to true.
+
+Note: The answer is guaranteed to fit within a 32-bit integer.
+*/
+
+
+class Solution {
+  public:
+    int solve(int i, int j, bool isT, string &s,vector<vector<vector<int>>>&dp){
+        if(i>j) return 0;
+        else if(i==j){
+            if(isT) return s[i]=='T';
+            else return s[i]=='F';
+        }
+        
+        if(dp[i][j][isT]!=-1) return dp[i][j][isT];
+        
+        int ways=0;
+        
+        for(int ind=i+1; ind<=j-1; ind+=2){
+            int lT=solve(i, ind-1, 1, s, dp);
+            int lF=solve(i, ind-1, 0, s, dp);
+            int rT=solve(ind+1, j, 1, s, dp);
+            int rF=solve(ind+1, j, 0, s, dp);
+            
+            if(isT){
+                if(s[ind]=='&'){
+                    ways+=(lT*rT);
+                }else if(s[ind]=='|'){
+                    ways+=((lT*rT)+(lT*rF)+(lF*rT));
+                }else{
+                    ways+=((lT*rF)+(lF*rT));
+                }
+            }else{
+                if(s[ind]=='&'){
+                    ways+=((lF*rF)+(lT*rF)+(lF*rT));
+                }else if(s[ind]=='|'){
+                    ways+=(lF*rF);
+                }else{
+                    ways+=((lT*rT)+(lF*rF));
+                }
+            }
+        }
+        return dp[i][j][isT]=ways;
+    }
+  
+    int countWays(string &s) {
+        // code here
+        int n=s.length();
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(n, vector<int>(2, -1)));
+        return solve(0, n-1, 1, s, dp);
+    }
+};
