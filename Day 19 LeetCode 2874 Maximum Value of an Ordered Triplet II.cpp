005c18ee12bd2724d3177ea8798f30996fadcd30
shivakant kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-04-03 21:35:45 +0530

    Create Day 19 LeetCode 2874 Maximum Value of an Ordered Triplet II.cpp

---
+++ Day 19 LeetCode 2874 Maximum Value of an Ordered Triplet II.cpp
@@ -0,0 +1,27 @@
+/*
+2874. Maximum Value of an Ordered Triplet II
+Solved
+Medium
+You are given a 0-indexed integer array nums.
+Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+ 
+Example 1:
+Input: nums = [12,6,1,2,7]
+Output: 77
+Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+It can be shown that there are no ordered triplets of indices with a value greater than 77. 
+*/
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        long maxTriplet = 0, maxElement = 0, maxDiff = 0;
+        for (long num : nums) {
+            maxTriplet = max(maxTriplet, maxDiff * num);
+            maxDiff = max(maxDiff, maxElement - num);
+            maxElement = max(maxElement, num);
+        }
+        return maxTriplet;
+    }
+};
