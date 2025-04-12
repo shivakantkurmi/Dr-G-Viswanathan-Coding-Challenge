6f015a04f86093a966cfa5fcd4470d1210a8f931
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-04-12 21:46:22 +0530

    Create Day 28 LeetCode 5 Longest Palindromic Substring.cpp

---
+++ Day 28 LeetCode 5 Longest Palindromic Substring.cpp
@@ -0,0 +1,49 @@
+/*
+5. Longest Palindromic Substring
+Solved
+Medium
+Given a string s, return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+*/
+
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) {
+            return "";
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int odd = expandAroundCenter(s, i, i);
+            int even = expandAroundCenter(s, i, i + 1);
+            int max_len = max(odd, even);
+
+            if (max_len > end - start) {
+                start = i - (max_len - 1) / 2;
+                end = i + max_len / 2;
+            }
+        }
+
+        return s.substr(start, end - start + 1);        
+    }
+
+private:
+    int expandAroundCenter(string s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }    
+};
