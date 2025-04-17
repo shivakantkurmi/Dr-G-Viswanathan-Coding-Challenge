d8ca20adc2bd64b6ecd7fe25ada397a80a6b45e7
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-04-17 21:45:20 +0530

    Create Day 33 GFG Minimum Weight Cycle.cpp

---
+++ Day 33 GFG Minimum Weight Cycle.cpp
@@ -0,0 +1,66 @@
+/*
+Minimum Weight Cycle
+Difficulty: HardAccuracy: 66.08%Submissions: 8K+Points: 8
+Given an undirected, weighted graph with V vertices numbered from 0 to V-1 and E edges, represented by a 2d array edges[][], where edges[i] = [u, v, w] represents the edge between the nodes u and v having w edge weight.
+Your task is to find the minimum weight cycle in this graph.
+
+Examples:
+
+Input: V = 5, edges[][] = [[0, 1, 2], [1, 2, 2], [1, 3, 1], [1, 4, 1], [0, 4, 3], [2, 3, 4]]
+
+Output: 6
+Explanation: 
+
+Minimum-weighted cycle is  0 → 1 → 4 → 0 with a total weight of 6(2 + 1 + 3)
+*/
+
+class Solution {
+  public:
+    int dijkstra(int src, int dest, int V, vector<vector<int>>adj[]){
+        vector<int>dist(V, 1e9);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+        pq.push({0, src});
+        dist[src]=0;
+        while(!pq.empty()){
+            auto &it=pq.top();
+            int node=it.second;
+            int d=it.first;
+            pq.pop();
+            
+            for(auto& it:adj[node]){
+                int adjN=it[0];
+                int adjW=it[1];
+                if((node==src && adjN==dest) || (node==dest && adjN==src)) continue;
+                if(d+adjW<dist[adjN]){
+                    dist[adjN]=d+adjW;
+                    pq.push({dist[adjN], adjN});
+                }
+            }
+        }
+        
+        return dist[dest];
+    }
+  
+    int findMinCycle(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>>adj[V];
+        for(auto &it:edges){
+            int u=it[0];
+            int v=it[1];
+            int w=it[2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+        
+        int mini=1e9;
+        
+        for(int i=0; i<edges.size(); i++){
+            int dist=(dijkstra(edges[i][0], edges[i][1], V, adj));
+            if(dist!=1e9){
+                mini=min(mini, dist+edges[i][2]);
+            }
+        }
+        
+        return mini;
+    }
+};
