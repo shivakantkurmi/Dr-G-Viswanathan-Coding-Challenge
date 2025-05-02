80dd6e14fc9b8c365c35d3c30a80ea86706325b5
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-05-02 19:33:35 +0530

    Create Extended Day 3 Fundamental .cpp

---
+++ Extended Day 3 Fundamental .cpp
@@ -0,0 +1,34 @@
+/*
+507. Perfect Number
+Solved
+Easy
+
+A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+Given an integer n, return true if n is a perfect number, otherwise return false.
+
+ 
+Example 1:
+Input: num = 28
+Output: true
+Explanation: 28 = 1 + 2 + 4 + 7 + 14
+1, 2, 4, 7, and 14 are all divisors of 28.
+
+Example 2:
+Input: num = 7
+Output: false
+ 
+Constraints:
+1 <= num <= 108
+*/
+
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0)sum+=i;
+            if(num<sum)return false;
+        }
+        return sum==num?1:0;
+    }
+};
