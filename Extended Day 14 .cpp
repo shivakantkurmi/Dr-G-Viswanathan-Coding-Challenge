55de2954e0beee7ba4711136f1dd72a98e0a419a
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-05-13 17:20:40 +0530

    Create Extended Day 14 .cpp

---
+++ Extended Day 14 .cpp
@@ -0,0 +1,49 @@
+/*
+86. Partition List
+Solved
+Medium
+
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+You should preserve the original relative order of the nodes in each of the two partitions.
+*/
+
+//Link : https://leetcode.com/problems/partition-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* slist = new ListNode(0, nullptr);
+        ListNode* blist = new ListNode(0, nullptr);
+        ListNode* small = slist;
+        ListNode* big = blist;
+
+        while (head != nullptr) {
+            if (head->val < x) {
+                small->next = head;
+                small = small->next;
+            } else {
+                big->next = head;
+                big = big->next;
+            }
+            head = head->next;
+        }
+
+        small->next = blist->next;
+        big->next = nullptr;
+
+        ListNode* result = slist->next;
+        delete slist;
+        delete blist;
+        return result;        
+    }
+};
