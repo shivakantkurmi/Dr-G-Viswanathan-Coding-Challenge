8b15f7efe28af38f3e6b6cb89b2e85ca752da6cc
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-05-16 17:01:02 +0530

    Create Extended Day 17 .cpp

---
+++ Extended Day 17 .cpp
@@ -0,0 +1,48 @@
+/*
+155. Min Stack
+Solved
+Medium
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+*/
+
+//Link : https://leetcode.com/problems/min-stack/description/
+
+class MinStack {
+private:
+    vector<vector<int>> st;
+
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        int min_val = getMin();
+        if (st.empty() || min_val > val) {
+            min_val = val;
+        }
+        st.push_back({val, min_val});        
+    }
+    
+    void pop() {
+        st.pop_back();
+    }
+    
+    int top() {
+        return st.empty() ? -1 : st.back()[0];
+    }
+    
+    int getMin() {
+        return st.empty() ? -1 : st.back()[1]; 
+    }
+};
