984b6d2d9908cfe308409d3824c8f2488f3017a2
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-05-16 17:02:05 +0530

    Create Extended Day 17 Easy.cpp

---
+++ Extended Day 17 Easy.cpp
@@ -0,0 +1,33 @@
+/*
+1. Two Sum
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+//Link : https://leetcode.com/problems/two-sum/description/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; // No solution found
+    }
+};
