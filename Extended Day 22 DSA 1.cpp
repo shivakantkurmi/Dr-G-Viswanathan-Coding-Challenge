32204c997286347e90a3e6c0f4f2d0480eac5272
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-05-21 20:39:52 +0530

    Create Extended Day 22 DSA 1.cpp

---
+++ Extended Day 22 DSA 1.cpp
@@ -0,0 +1,37 @@
+/*
+1249. Minimum Remove to Make Valid Parentheses
+Solved
+Medium
+
+Given a string s of '(' , ')' and lowercase English characters.
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+*/
+
+//Link : https://leetcode.com/problems/majority-element/description/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
+    }
+};
