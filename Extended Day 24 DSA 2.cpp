cae23b756ab8bdee79318a482a0cb42e13b8aca6
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-05-23 21:40:28 +0530

    Create Extended Day 24 DSA 2.cpp

---
+++ Extended Day 24 DSA 2.cpp
@@ -0,0 +1,27 @@
+/*
+283. Move Zeroes
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+*/
+
+//Link : https://leetcode.com/problems/move-zeroes/description/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] != 0) {
+                swap(nums[right], nums[left]);
+                left++;
+            }
+        }        
+    }
+};
