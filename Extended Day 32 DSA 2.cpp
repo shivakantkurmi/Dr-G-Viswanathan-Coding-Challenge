2dd0f667f2d64f7df163988d7c9e69f4ee05d996
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-05-31 16:24:17 +0530

    Create Extended Day 32 DSA 2.cpp

---
+++ Extended Day 32 DSA 2.cpp
@@ -0,0 +1,28 @@
+/*
+48. Rotate Image
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+*/
+
+//Link : https://leetcode.com/problems/rotate-image/description/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        for(int i=0;i<row; i++){
+            for(int j=0; j<=i;j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i=0;i<row;i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
