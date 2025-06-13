d0273cee6f81c3485ed7a0f0cd1f13185846a64e
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2025-06-13 15:26:50 +0530

    Create Extended Day 45 DSA 1.cpp

---
+++ Extended Day 45 DSA 1.cpp
@@ -0,0 +1,29 @@
+/*
+997. Find the Town Judge
+Solved
+Easy
+
+In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+If the town judge exists, then:
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi. If a trust relationship does not exist in trust array, then such a trust relationship does not exist.
+
+Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+*/
+
+//Link : https://leetcode.com/problems/find-the-town-judge/description/
+
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        vector<int> count(N + 1, 0);
+        for (auto& t : trust)
+            count[t[0]]--, count[t[1]]++;
+        for (int i = 1; i <= N; ++i) {
+            if (count[i] == N - 1) return i;
+        }
+        return -1;
+    }
+};
